69877db46a12eb60b240ffe8c1056d15fd47f633
deepdas2607 <deepdas2607@gmail.com>
2024-08-24 14:40:46 +0530

    added level to story mode

--- level.gd
+++ level.gd
@@ -4,3 +4,7 @@ extends Control
 
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://mode.tscn")
+
+
+func _on_lvl_pressed() -> void:
+	get_tree().change_scene_to_file("")
