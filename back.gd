f32601707481433238f960afab55092b40beebab
deepdas2607 <deepdas2607@gmail.com>
2024-08-27 21:48:21 +0530

    created a pause screen press esc to access

---
+++ back.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://level.tscn")
+
+func _ready():
+	Dialogic.start("lawyergametimeline")
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func testEsc():
+	if Input.is_action_just_pressed("esc") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused:
+		resume()
+
+func _process(delta):
+	testEsc()
