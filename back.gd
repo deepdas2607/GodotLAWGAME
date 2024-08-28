dd705de77115695bf022f84f29cd24e90ba2be3f
deepdas2607 <deepdas2607@gmail.com>
2024-08-28 22:06:56 +0530

    made choice button instead of string

--- back.gd
+++ back.gd
@@ -5,7 +5,7 @@ func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://level.tscn")
 
 func _ready():
-	Dialogic.start("lawyergametimeline")
+	Dialogic.start("timeline")
 	$AnimationPlayer.play("RESET")
 
 func resume():
