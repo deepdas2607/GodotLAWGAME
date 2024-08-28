3502752667ddefbc332262d4a277ac2bb248aa1a
deepdas2607 <deepdas2607@gmail.com>
2024-08-28 17:47:15 +0530

    comming soon'

--- level.gd
+++ level.gd
@@ -8,3 +8,23 @@ func _on_texture_button_pressed() -> void:
 
 func _on_lvl_pressed() -> void:
 	get_tree().change_scene_to_file("res://lvl1.tscn")
+
+
+func _on_texture_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://comingsoon.tscn")
+
+
+func _on_texture_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://comingsoon.tscn")
+
+
+func _on_lvlfour_pressed() -> void:
+	get_tree().change_scene_to_file("res://comingsoon.tscn")
+
+
+func _on_lvlfive_pressed() -> void:
+	get_tree().change_scene_to_file("res://comingsoon.tscn")
+
+
+func _on_lvlsix_pressed() -> void:
+	get_tree().change_scene_to_file("res://comingsoon.tscn")
--- minigamemenu.gd
+++ minigamemenu.gd
@@ -4,3 +4,11 @@ extends Control
 
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://mode.tscn")
+
+
+func _on_story_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://comingsoon.tscn")
+
+
+func _on_texture_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://comingsoon.tscn")
